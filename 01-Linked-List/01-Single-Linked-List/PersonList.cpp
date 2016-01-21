1989145410c9eb694a473a8bdddc860b2a7a0769
Çağatay Erdiz <cagatay_erdiz@hotmail.com>
2016-01-21 05:56:12 +0200

    Create PersonList.cpp

---
+++ 01-Linked-List/01-Single-Linked-List/PersonList.cpp
@@ -0,0 +1,83 @@
+#include "PersonList.h"
+
+PersonList::PersonList()
+{
+	head = NULL;
+	recordCount = 0;
+}
+
+void PersonList::Insert(char *name, char *surname, int age)
+{
+	// allocation for a Person class
+	Person *newNode = new Person();
+	newNode->name = name;
+	newNode->surname = surname;
+	newNode->age = age;
+	newNode->next = NULL;
+
+	if (head == NULL)
+	{
+		head = newNode;
+		recordCount++;
+		return;
+	}
+
+	Person *traverse = head;
+
+	while (traverse->next)
+		traverse = traverse->next;
+
+	// newNode added at the end of the list
+	traverse->next = newNode;
+	recordCount++;
+}
+
+void PersonList::Remove(int no)
+{
+	Person *tail = NULL, *traverse = head;
+	int i = 1;
+	
+	// if head is removed
+	if (no == 1)
+	{
+		head = head->next;
+		delete traverse;
+		recordCount--;
+		return;
+	}
+
+	// else
+	while (i < no)
+	{
+		tail = traverse;
+		traverse = traverse->next;
+		i++;
+	}
+
+	tail->next = traverse->next;
+	
+	delete traverse;
+	recordCount--;
+}
+
+void PersonList::List()
+{
+	// for not losing head pointer
+	Person *traverse = head;
+	int i = 1;
+
+	while (traverse)
+	{
+		cout << " " << i << ". " << traverse->name << " "
+			<< traverse->surname << " " << traverse->age << endl;
+
+		traverse = traverse->next;
+		i++;
+	}
+}
+
+PersonList::~PersonList()
+{
+	//MakeEmpty();
+	recordCount = 0;
+}
